4fd7c6ea0c8d60e99552b2eebb70aae2d169b000
Nicoleta Olaru <olarunicoleta783@gmail.com>
2021-05-23 04:07:20 +0200

    test

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class Account(models.Model):
+class Account(AbstractBaseUser):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, unique=True)
-    username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=50)
     role = models.CharField(max_length=50)
     school = models.CharField(max_length=50)
     programme = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+    username = None
+
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.name + " " + self.surname
