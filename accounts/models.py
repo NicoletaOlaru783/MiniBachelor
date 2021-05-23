2cc13a919bbc759aa551fc4858fea72b84acc46a
Nicoleta Olaru <olarunicoleta783@gmail.com>
2021-05-23 09:53:22 +0200

    Account changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,49 +1,73 @@
-from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-
-from django.contrib.auth.hashers import make_password
+# Create your models here.
 
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, password, alias=None):
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,)
-        user.set_password(make_password(password))
-        user.save()
-        return user
+    def create_user(self, name, surname, username, email, password=None):
+        if not email:
+            raise ValueError('Email address is required')
+        if not username:
+            raise ValueError('Username is required')
 
-    def create_superuser(self, email, username, password):
         user = self.model(
             email=self.normalize_email(email),
             username=username,
-            password=password,
+            name=name,
+            surname=surname,
         )
-        user.is_staff()
-        user.is_superuser = True
-        user.save()
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, surname, username, email, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            name=name,
+            surname=surname,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self.db)
         return user
 
 
 class Account(AbstractBaseUser):
-    username = models.CharField(max_length=255)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     role = models.CharField(max_length=50)
     school = models.CharField(max_length=50)
     programme = models.CharField(max_length=50)
+
+    # required
     created_at = models.DateTimeField(auto_now_add=True)
-    username = None
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
 
-    email = models.EmailField(('email address'), unique=True)
-
+    # set login field
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'name', 'surname']
 
+    # at the end we need to tell the Account that we are using
+    # MyAccountManager for all this operations
     objects = MyAccountManager()
 
     def __str__(self):
-        return self.name + " " + self.surname
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
