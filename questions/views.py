ac4a85d4b61f54bff64e5dfad3f90f5336f3a274
Nicoleta Olaru <olarunicoleta783@gmail.com>
2021-05-25 20:43:29 +0200

    jj

--- questions/views.py
+++ questions/views.py
@@ -31,6 +31,6 @@ class QuestionViewSet(viewsets.ModelViewSet):
         if school is not None:
             queryset = Account.objects.filter(school=school)
 
-        serializer_class = QuestionSerializer(queryset, many=True)
+        serializer_class = QuestionSerializer
 
         return queryset
