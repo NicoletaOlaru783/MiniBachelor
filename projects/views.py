d623ac1045eaa0aff67c8a43548b30834d772231
Nicoleta Olaru <olarunicoleta783@gmail.com>
2021-05-25 22:06:00 +0200

    test

--- projects/views.py
+++ projects/views.py
@@ -16,10 +16,24 @@ class ProjectViewSet(viewsets.ModelViewSet):
         id = self.request.query_params.get('id')
         user = self.request.query_params.get('user')
 
+        # fields from user
+        school = self.request.query_params.get('school')
+        programme = self.request.query_params.get('programme')
+
         if id is not None:
             queryset = queryset.filter(id=id)
         if user is not None:
             queryset = queryset.filter(user=user)
+
+        # fields from user
+        if school is not None:
+            user = self.request.user.school
+            queryset = queryset.filter(user__school=school)
+
+        # fields from user
+        if programme is not None:
+            user = self.request.user.programme
+            queryset = queryset.filter(user__programme=programme)
 
         return queryset
 
