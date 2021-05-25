88618e73637d3af17e08899ab29d9cf4898072b1
Nicoleta Olaru <olarunicoleta783@gmail.com>
2021-05-25 18:20:53 +0200

    api changes

--- accounts/views.py
+++ accounts/views.py
@@ -13,11 +13,14 @@ class AccountViewSet(viewsets.ModelViewSet):
 
     serializer_class = AccountSerializer
 
-    def retrieve(self, request, *args, **kwargs):
-        # Check if user is authenticated
-        permission_classes = (IsAuthenticated,)
-
+    def get_queryset(self, request):
         queryset = Account.objects.all()
+        method = self.request.method
+
+        if method == 'GET':
+            permission_classes = (IsAuthenticated,)
+        if method == 'POST':
+            permission_classes = [permissions.AllowAny]
 
         # Query tags allowed
         id = self.request.query_params.get('id')
@@ -34,18 +37,7 @@ class AccountViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(programme=programme)
         if role is not None:
             queryset = queryset.filter(role=role)
-        if school is not None:
+        if programme is not None:
             school = queryset.filter(school=school)
 
         return queryset
-
-    def create(self, request, *args, **kwargs):
-        # Check if user is authenticated
-        permission_classes = [
-            permissions.AllowAny
-        ]
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
